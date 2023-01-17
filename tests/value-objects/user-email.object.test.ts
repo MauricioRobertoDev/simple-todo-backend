f3f2545ef2317e9d21250c9eda1a5a43e5d3b65b
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-01-17 17:03:34 -0300

    adicionado testes novos para completar o user email

--- tests/value-objects/user-email.object.test.ts
+++ tests/value-objects/user-email.object.test.ts
@@ -3,11 +3,14 @@ import { describe, expect, test } from "vitest";
 import { ErrorBundle } from "@/shared/error-bundle";
 import { UserEmail } from "@/objects/user-email.object";
 import { Message } from "@/util/messages";
+import { Left, Right } from "@/errors/either";
 
 describe("UserEmail", () => {
   test("Deve retornar um UserEmail com valor formatado corretamente", () => {
     const emailOrErrors = UserEmail.create("VaLid_EmAiL@dOmain.com");
     expect(emailOrErrors.isRight()).toBeTruthy();
+    expect(emailOrErrors.isLeft()).toBeFalsy();
+    expect(emailOrErrors).toBeInstanceOf(Right);
     expect(emailOrErrors.getValue()).toBeInstanceOf(UserEmail);
     expect((emailOrErrors.getValue() as UserEmail).value).toBe(
       "valid_email@domain.com"
@@ -17,6 +20,8 @@ describe("UserEmail", () => {
   test("Deve retornar um ErrorBundle caso passe email vazio", () => {
     let emailOrErrors = UserEmail.create("");
     expect(emailOrErrors.isLeft()).toBeTruthy();
+    expect(emailOrErrors.isRight()).toBeFalsy();
+    expect(emailOrErrors).toBeInstanceOf(Left);
     expect(emailOrErrors.getValue()).toBeInstanceOf(ErrorBundle);
     expect(
       (emailOrErrors.getValue() as ErrorBundle).getErrors()[0].message
@@ -53,5 +58,15 @@ describe("UserEmail", () => {
     expect(
       (emailOrErrors.getValue() as ErrorBundle).getErrors()[0].message
     ).toBe(Message.EMAIL_TO_LONG);
+  });
+
+  test("Deve retornar um ErrorBundle caso um email inválido", () => {
+    const invalidEmail = "invalid_email@hotmail";
+    const emailOrErrors = UserEmail.create(invalidEmail);
+    expect(emailOrErrors.isLeft()).toBeTruthy();
+    expect(emailOrErrors.getValue()).toBeInstanceOf(ErrorBundle);
+    expect(
+      (emailOrErrors.getValue() as ErrorBundle).getErrors()[0].message
+    ).toBe(Message.EMAIL_INVALID);
   });
 });
