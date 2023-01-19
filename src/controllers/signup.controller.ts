402d470b1dc7db7a21198bc402cab6a1aeed1693
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-01-19 15:25:10 -0300

    criação do SignupController e arquivo index para centralização

---
+++ src/controllers/signup.controller.ts
@@ -0,0 +1,36 @@
+import { IController } from "@/interfaces";
+import { UserData, UserMapper } from "@/mappers/user.mapper";
+import { PrismaUserRepository } from "@/repositories/prisma";
+import { CreateUserService } from "@/services";
+import { HttpStatus } from "@/util/http-status";
+import { Message } from "@/util/messages";
+import { Request, Response } from "express";
+
+export class SigninController implements IController {
+  async handle(req: Request, res: Response): Promise<Response> {
+    const userRepository = new PrismaUserRepository();
+    const createUserService = new CreateUserService(userRepository);
+
+    const userDataOrErrors = await createUserService.execute({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    if (userDataOrErrors.isLeft()) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        message: Message.VALIDATION_ERRORS,
+        errors: userDataOrErrors.getValue().getErrorData(),
+      });
+    }
+
+    const userData: Omit<UserData, "password"> = UserMapper.toDTO(
+      userDataOrErrors.getValue()
+    );
+
+    return res.status(HttpStatus.CREATED).json({
+      message: Message.CREATED_ACCOUNT,
+      data: userData,
+    });
+  }
+}
