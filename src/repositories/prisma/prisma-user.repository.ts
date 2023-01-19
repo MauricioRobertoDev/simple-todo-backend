8a181ac19f6ac4708927b3ff4e2a2958f7e096ad
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-01-19 15:24:07 -0300

    criação do PrismaUserRepository e arquivo index para centralizar os repositórios

---
+++ src/repositories/prisma/prisma-user.repository.ts
@@ -0,0 +1,61 @@
+import { client } from "@/database/prisma.client";
+import { User } from "@/entities";
+import { UserRepository } from "../interfaces";
+
+export class PrismaUserRepository implements UserRepository {
+  async findById(id: string): Promise<User | null> {
+    const userModel = await client.userModel.findFirst({ where: { id } });
+
+    if (userModel) {
+      const user = User.create(
+        {
+          name: userModel.name,
+          email: userModel.email,
+          password: userModel.password,
+        },
+        userModel.id
+      );
+
+      if (user.isLeft()) return null;
+
+      return user.getValue();
+    }
+
+    return null;
+  }
+  async findByEmail(email: string): Promise<User | null> {
+    const userModel = await client.userModel.findFirst({ where: { email } });
+
+    if (userModel) {
+      const user = User.create(
+        {
+          name: userModel.name,
+          email: userModel.email,
+          password: userModel.password,
+        },
+        userModel.id
+      );
+
+      if (user.isLeft()) return null;
+
+      return user.getValue();
+    }
+
+    return null;
+  }
+  async exists(email: string): Promise<boolean> {
+    const userModel = await client.userModel.findFirst({ where: { email } });
+
+    return userModel ? true : false;
+  }
+  async save(user: User): Promise<void> {
+    await client.userModel.create({
+      data: {
+        id: user.id,
+        name: user.name.getValue(),
+        email: user.email.getValue(),
+        password: user.password.getValue(),
+      },
+    });
+  }
+}
