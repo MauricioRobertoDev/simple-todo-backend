69a07850ebea04cb3311bcb47e0d748fbc8a1389
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-01-21 15:06:28 -0300

    alteração no setup de testes e2e para evitar conflitos

--- e2e-setup.ts
+++ e2e-setup.ts
@@ -1,18 +1,18 @@
 import pg from "pg";
 import crypto from "crypto";
 import { execSync } from "child_process";
-import { afterAll, beforeAll } from "vitest";
+import { afterEach, beforeEach } from "vitest";
 
 const schema = `test_${crypto.randomUUID()}`;
-const connectionString = `${process.env.DATABASE_URL}?schema${schema}`;
+const connectionString = `${process.env.DATABASE_URL}?schema=${schema}`;
 process.env.DATABASE_URL = connectionString;
 global.process.env.DATABASE_URL = connectionString;
 
-beforeAll(async () => {
+beforeEach(async () => {
   await execSync(`./node_modules/.bin/prisma migrate deploy`);
 });
 
-afterAll(async () => {
+afterEach(async () => {
   const client = new pg.Client({ connectionString });
   await client.connect();
   await client.query(`DROP SCHEMA IF EXISTS "${schema}" CASCADE`);
