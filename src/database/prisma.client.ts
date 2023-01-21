ddd27d8314d5d2d8d3bd381aa57dbe4f8716f5d8
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-01-21 18:52:36 -0300

    removido os logs, e deixado em forma de comentário para visualização rápida de logs

--- src/database/prisma.client.ts
+++ src/database/prisma.client.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient({ log: ["query", "info", "warn"] });
+const prisma = new PrismaClient();
+// const prisma = new PrismaClient({ log: ["query", "warn"] });
 
 export default prisma;
