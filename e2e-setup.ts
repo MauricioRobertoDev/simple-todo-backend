eea20b14fe0a2797b73ac9f1f9a133a969cd3173
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-01-21 18:51:46 -0300

    setado beforeAll e afterAll

--- e2e-setup.ts
+++ e2e-setup.ts
@@ -8,11 +8,11 @@ const connectionString = `${process.env.DATABASE_URL}?schema=${schema}`;
 process.env.DATABASE_URL = connectionString;
 global.process.env.DATABASE_URL = connectionString;
 
-beforeEach(async () => {
+beforeAll(async () => {
   await execSync(`./node_modules/.bin/prisma migrate deploy`);
 });
 
-afterEach(async () => {
+afterAll(async () => {
   const client = new pg.Client({ connectionString });
   await client.connect();
   await client.query(`DROP SCHEMA IF EXISTS "${schema}" CASCADE`);
