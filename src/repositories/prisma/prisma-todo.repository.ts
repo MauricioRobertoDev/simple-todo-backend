e2c27e7e30459f9aaf12b5fac42efb1a13300409
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-01-22 17:48:04 -0300

    criação do PrismaTodoRepository

---
+++ src/repositories/prisma/prisma-todo.repository.ts
@@ -0,0 +1,68 @@
+import { Todo } from "@/entities";
+import prisma from "@/database/prisma.client";
+import { TodoRepository } from "../interfaces";
+
+export class PrismaTodoRepository implements TodoRepository {
+  async findAllByUserId(userId: string): Promise<Todo[]> {
+    const todoModels = await prisma.todoModel.findMany({
+      where: { ownerId: userId },
+    });
+
+    const todos: Todo[] = [];
+
+    if (todoModels.length > 0) {
+      todoModels.forEach((todoModel) => {
+        todos.push(
+          Todo.create(
+            {
+              description: todoModel.description,
+              createdAt: todoModel.createdAt,
+              ownerId: todoModel.ownerId,
+              startAt: todoModel.startAt ?? undefined,
+              endAt: todoModel.endAt ?? undefined,
+            },
+            todoModel.id
+          ).getValue() as Todo
+        );
+      });
+    }
+
+    return todos;
+  }
+
+  async findById(id: string): Promise<Todo | null> {
+    const todoModel = await prisma.todoModel.findFirst({ where: { id } });
+
+    if (todoModel) {
+      const user = Todo.create(
+        {
+          description: todoModel.description,
+          createdAt: todoModel.createdAt,
+          ownerId: todoModel.ownerId,
+          startAt: todoModel.startAt ?? undefined,
+          endAt: todoModel.endAt ?? undefined,
+        },
+        todoModel.id
+      );
+
+      if (user.isLeft()) return null;
+
+      return user.getValue();
+    }
+
+    return null;
+  }
+
+  async save(todo: Todo): Promise<void> {
+    await prisma.todoModel.create({
+      data: {
+        description: todo.description.getValue(),
+        createdAt: todo.createdAt,
+        ownerId: todo.ownerId,
+        startAt: todo.startAt,
+        endAt: todo.endAt,
+        id: todo.id,
+      },
+    });
+  }
+}
