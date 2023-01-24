d08c85050daf0e4cccb5e3c05b7e238726a0b3a6
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-01-24 18:01:54 -0300

    criação da rota para atualizar status da todo

--- src/routes/todo.routers.ts
+++ src/routes/todo.routers.ts
@@ -1,5 +1,6 @@
 import { CreateTodoController } from "@/controllers/create-todo.controller";
 import { EditTodoController } from "@/controllers/edit-todo.controller";
+import { UpdateStatusTodoController } from "@/controllers/update-todo-status.controller";
 import { auth } from "@/middlewares/auth.middleware";
 import { Router } from "express";
 
@@ -7,5 +8,10 @@ const todoRouter = Router();
 
 todoRouter.post("/nova-todo", auth, new CreateTodoController().handle);
 todoRouter.post("/editar-todo/:id", auth, new EditTodoController().handle);
+todoRouter.post(
+  "/atualizar-status-todo/:id",
+  auth,
+  new UpdateStatusTodoController().handle
+);
 
 export default todoRouter;
