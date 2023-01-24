971b76a7f47f046e35274ebf5219a741ed7302e7
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-01-24 16:54:26 -0300

    criação da rota para editar uma todo

--- src/routes/todo.routers.ts
+++ src/routes/todo.routers.ts
@@ -1,9 +1,11 @@
 import { CreateTodoController } from "@/controllers/create-todo.controller";
+import { EditTodoController } from "@/controllers/edit-todo.controller";
 import { auth } from "@/middlewares/auth.middleware";
 import { Router } from "express";
 
 const todoRouter = Router();
 
 todoRouter.post("/nova-todo", auth, new CreateTodoController().handle);
+todoRouter.post("/editar-todo/:id", auth, new EditTodoController().handle);
 
 export default todoRouter;
