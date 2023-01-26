8acfd1f5ff641c03e2d54ad9b7a470f2b302bbd8
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-01-26 12:29:28 -0300

    adição dos novos serviços ao index

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,3 +2,5 @@ export * from "./create-user.service";
 export * from "./authenticate-user.service";
 export * from "./create-todo.service";
 export * from "./auth.service";
+export * from "./edit-todo.service";
+export * from "./update-todo-status.service";
