5ecbe6eff8417bb76b8cd80870f7353454db50b2
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-01-26 17:17:10 -0300

    adicionado DeleteTodoService

--- src/services/index.ts
+++ src/services/index.ts
@@ -5,3 +5,4 @@ export * from "./auth.service";
 export * from "./edit-todo.service";
 export * from "./update-todo-status.service";
 export * from "./list-todo.service";
+export * from "./delete-todo.service";
