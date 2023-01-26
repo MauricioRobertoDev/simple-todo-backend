bd180063dc600ef77774f3d4e564019e3f32233e
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-01-26 20:05:33 -0300

    adicionado um error handler para pegar erros inesperados

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
-import express from "express";
+import "express-async-errors";
+import express, { NextFunction, Request, Response } from "express";
 import authRouter from "@/routes/auth.routers";
 import todoRouter from "@/routes/todo.routers";
+import { Message } from "./util/messages";
 
 const app = express();
 
@@ -8,5 +10,12 @@ app.use(express.json());
 
 app.use(authRouter);
 app.use(todoRouter);
+
+app.use(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    response.status(500).send(Message.SERVER_ERROR);
+  }
+);
 
 export { app };
