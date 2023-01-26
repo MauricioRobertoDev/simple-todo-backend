b7e375c6477d1d298cfbd5032e5fb128e0467743
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-01-26 16:43:46 -0300

    adição dos controllers faltantes ao aquivo index

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,2 +1,6 @@
 export * from "./signin.controller";
 export * from "./signup.controller";
+export * from "./create-todo.controller";
+export * from "./update-todo-status.controller";
+export * from "./edit-todo.controller";
+export * from "./list-todo.controller";
